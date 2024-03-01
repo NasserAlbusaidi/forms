cb81d8d636f2b7029338c762b94f8a28b9abbc4d
NasserAlbusaidi <nasserbusaidi@gmail.com>
2024-03-01 15:43:27 +0400

    Remove unused environment variable and update API endpoint

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -29,7 +29,6 @@ const FormComponent = () => {
     39, 40,
   ];
   const speed = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
-  const sheetmonkeyKey = process.env.REACT_APP_SHEETMONKEY_API_KEY;
   const handleSpeedChange = (event, newValue) => {
     setSpeedValue(newValue);
     setFormData((prevData) => ({
@@ -58,7 +57,7 @@ const FormComponent = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(`https://api.sheetmonkey.io/form/${sheetmonkeyKey}`, {
+    const response = await fetch(`https://api.sheetmonkey.io/form/no4zi7XKpRriFV2kXeLWRz`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
